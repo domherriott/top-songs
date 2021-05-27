38c62b5e08f8a3b2a3a5533cc432ec78d6adba2b
Dominic Herriott <dherriott@deloitte.co.uk>
2021-05-27 09:38:25 +0100

    Updated bash script

--- spotify_extract/get_tracks.sh
+++ spotify_extract/get_tracks.sh
@@ -1,19 +1,30 @@
-#!/bin/bash
+#!/bin/zsh
 
-echo Starting Spotify extract...
+echo -e "\nStarting Spotify extract...\n"
+echo Date:
+date
 
+echo -e "\n"
 cd /Users/dherriott/Documents/top-songs/spotify_extract
 
+echo -e "\n"
 git checkout main
+
+echo -e "\n"
 git fetch origin main
 
+echo -e "\n"
 python3 get_tracks.py
 
+echo -e "\n"
 git add ../src/tracks/*
+
+echo -e "\n"
 git commit -m "$DATE - Spotify extract"
 
+echo -e "\n"
 git push origin main
 
-echo Spotify extract complete!
-
 cd ~
+
+echo -e "\nSpotify extract complete!\n"
